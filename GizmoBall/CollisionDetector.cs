932a1251438436bd2e44ff253c6367b56ee25f17
SouthStreet <znj764359824@gmail.com>
2015-11-18 23:12:31 +0800

    quadtree

---
+++ GizmoBall/CollisionDetector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GizmoBall
+{
+    class CollisionDetector
+    {
+        public bool detect(Ball ball, BumperCir cir)
+        {
+            bool isCollision = false;
+
+            return isCollision;
+        }
+        public bool detect(Ball ball,BumperSqr sqr)
+        {
+            bool isCollision = false;
+
+            return isCollision;
+        }
+
+        public bool detect(Ball ball,BumperTrg trg )
+        {
+            bool isCollision = false;
+
+            return isCollision;
+        }
+        public bool detect(Ball ball, Flipper flipper)
+        {
+            bool isCollision = false;
+
+            return isCollision;
+        }
+
+
+    }
+}
